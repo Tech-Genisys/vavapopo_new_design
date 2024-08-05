bb72fcf21d38dc011f29f902075725c52b2d3b65
Kethankrk <kethankrk@gmail.com>
2024-08-05 18:35:22 +0530

    included blogs in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,25 +1,42 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase/firebaseinit";
 
-const getAllPackageUrls = async () => {
-    const packages = await getDocs(collection(db, "packages"))
-    const packagesUrl = []
-    packages.forEach(item => {
-        const data = item.data()
-        packagesUrl.push({
-            url: `${process.env.NEXT_PUBLIC_DOMAIN}/trip/${item.id}`,
-            lastModified: data.date,
-            changeFrequency: "yearly",
-            priority: 1
-        })
-    })
+export const revalidate = 30;
 
-    return packagesUrl
-}
-export const revalidate = 30
+const getAllPackageUrls = async () => {
+  const packages = await getDocs(collection(db, "packages"));
+  const packagesUrl = [];
+  packages.forEach((item) => {
+    const data = item.data();
+    packagesUrl.push({
+      url: `${process.env.NEXT_PUBLIC_DOMAIN}/trip/${item.id}`,
+      lastModified: data.date,
+      changeFrequency: "yearly",
+      priority: 1,
+    });
+  });
+
+  return packagesUrl;
+};
+
+const getAllBlogsUrls = async () => {
+  const blogs = await getDocs(collection(db, "blogs"));
+  const blogsUrl = [];
+  blogs.forEach((item) => {
+    const data = item.data();
+    blogsUrl.push({
+      url: `${process.env.NEXT_PUBLIC_DOMAIN}/trip/${item.id}`,
+      lastModified: data.date,
+      changeFrequency: "monthly",
+      priority: 1,
+    });
+  });
+
+  return blogsUrl;
+};
 
 export default async function sitemap() {
-  const ulrs =  [
+  const ulrs = [
     {
       url: process.env.NEXT_PUBLIC_DOMAIN,
       lastModified: new Date(),
@@ -27,14 +44,21 @@ export default async function sitemap() {
       priority: 1,
     },
     {
-        url: `${process.env.NEXT_PUBLIC_DOMAIN}/packs`,
-        lastModified: new Date(),
-        changeFrequency: "monthly",
-        priority: 1,
+      url: `${process.env.NEXT_PUBLIC_DOMAIN}/packs`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_DOMAIN}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
   ];
 
-  ulrs.push(...(await getAllPackageUrls()))
+  ulrs.push(...(await getAllPackageUrls()));
+  ulrs.push(...(await getAllBlogsUrls()));
 
-  return ulrs
+  return ulrs;
 }
