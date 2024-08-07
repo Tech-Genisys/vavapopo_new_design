cee67cba7f2d7a502d7cc0ac93bd68ac55131e39
Kethankrk <kethankrk@gmail.com>
2024-08-07 10:14:20 +0530

    fixed conflict

--- app/(home)/blog/componets/blogcard.jsx
+++ app/(home)/blog/componets/blogcard.jsx
@@ -42,35 +42,13 @@ const Blogcard = ({
         <div className=" flex flex-col">
           <div className="flex gap-2">
             {tags.map((item, index) => {
-              if(index > 2) return
+              if (index > 2) return;
               return (
                 <p key={index} className="bg-background px-2 md:px-3 font-semibold rounded-full text-[10px] md:text-base mb-1 md:mb-3">
                   {item}
                 </p>
               );
             })}
-        </div>
-        <h1 className="text-black text-lg sm:text-3xl font-semibold max-w-3xl">
-          {title}
-        </h1>
-        <div className="flex sm:gap-3 mt-3 text-xs sm :text-sm gap-1">
-          <div className="flex gap-1 text-gray-800 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-4 sm:size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-              />
-            </svg>
-
-            <p className="font-medium ">{date}</p>
           </div>
           <h1 className="text-black md:text-2xl font-semibold max-w-3xl">
             {title}
