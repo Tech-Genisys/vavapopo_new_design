c23b6388328109dbb38426c173b0af2f180bb140
Kethankrk <kethankrk@gmail.com>
2024-09-01 12:18:22 +0530

    feat: added herosection image as opengraph image in home page

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -13,6 +13,10 @@ export const metadata = {
   },
   description:
     "Dive into cultural, and heritage tours. Experience the warmth of local hospitality, explore rich traditions.",
+  openGraph: {
+    images:
+      "https://firebasestorage.googleapis.com/v0/b/vavapopo-ad842.appspot.com/o/vavapopo-herosection.png?alt=media&token=018b2dd1-1565-4328-9216-a8b9b514c442",
+  },
 };
 
 export default function RootLayout({ children }) {
